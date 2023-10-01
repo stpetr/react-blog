ee7afae564feb4d8d80ce8115c13cf3e2a139c2b
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-10-01 17:22:01 +0700

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
+}
