2d133873a2d2e5c0ed889f4ce10a9ee182d45882
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-11-10 20:08:14 +0700

    Add AppButton component

---
+++ src/shared/ui/app-button/index.tsx
@@ -0,0 +1,27 @@
+import { ButtonHTMLAttributes, FC } from 'react'
+
+import cn from 'shared/helpers/class-names'
+
+import styles from './app-button.module.scss'
+
+export enum AppButtonTheme {
+  CLEAR = 'clear',
+}
+
+interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  className?: string
+  theme?: AppButtonTheme
+}
+
+export const AppButton: FC<AppButtonProps> = (props) => {
+  const {
+    className,
+    theme,
+    children,
+    ...restProps
+  } = props
+
+  return (
+    <button className={cn(styles.appButton, styles[theme], className)} {...restProps}>{children}</button>
+  )
+}
