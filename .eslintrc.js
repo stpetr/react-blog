b828a6da171bd72dda3100afcc3c704662175872
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2023-11-16 20:23:54 +0700

    Alter eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,8 @@ module.exports = {
     "@typescript-eslint/strict-boolean-expressions": "off",
     "@typescript-eslint/comma-dangle": ["error", "only-multiline"],
     "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/space-before-function-paren": "off",
     "i18next/no-literal-string": ["error", { markupOnly: true }],
+    "no-console": ["error", { allow: ["warn", "error"] }],
   },
 }
