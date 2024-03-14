14bb8eea4297bf442545a993c5e263fe32ad7344
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2024-03-14 19:21:58 +0700

    Reformat linter's config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,57 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
+  env: {
+    browser: true,
+    es2021: true,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-  "extends": [
-    "standard-with-typescript",
-    "plugin:react/recommended",
-    "plugin:storybook/recommended"
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended'
   ],
-  "overrides": [
+  overrides: [
     {
-      "env": {
-        "node": true,
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}",
+      files: [
+        '.eslintrc.{js,cjs}',
       ],
-      "parserOptions": {
-        "sourceType": "script",
+      parserOptions: {
+        sourceType: 'script',
       },
     },
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": './tsconfig.json',
-    "tsconfigRootDir": __dirname,
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  "plugins": [
-    "react",
-    "i18next",
-    "react-hooks",
+  plugins: [
+    'react',
+    'i18next',
+    'react-hooks',
   ],
-  "rules": {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-exports": "off",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/comma-dangle": ["error", "only-multiline"],
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/space-before-function-paren": "off",
-    "i18next/no-literal-string": ["error", { markupOnly: true }],
-    "no-console": ["error", { allow: ["warn", "error"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-exports': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
 }
